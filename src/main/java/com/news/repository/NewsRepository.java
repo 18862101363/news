54d74e9f6789d347778f6319e0f8765c7b7f1a07
dong_zhengdong <dong_zhengdong@wuxiapptec.com>
2019-05-02 10:56:11 +0800

    init

--- src/main/java/com/news/repository/NewsRepository.java
+++ src/main/java/com/news/repository/NewsRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
  * Created by dong_zhengdong on 2019/4/29.
  *
  * https://blog.csdn.net/topdandan/article/details/81436141     springboot中ElasticSearch入门与进阶：组合查询、Aggregation聚合查询（你想要的都有）
+ * https://my.oschina.net/kipeng/blog/1799827
+ * https://segmentfault.com/a/1190000015568618
  */
 public interface NewsRepository extends ElasticsearchRepository<News, String> {
 
